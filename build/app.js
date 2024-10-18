8756287b9fa378c9e7a9c1341cce78dd812ab457
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-10-18 17:49:24 +0100

    Write banchmarking with tinybench

--- build/app.js
+++ build/app.js
@@ -1,8 +1,18 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const mergesort_1 = require("./mergesort");
 const bubblesort_1 = require("./bubblesort");
-const arr = [5, 4, 8, 10, 9, 3, 2, 1, 7, 6];
+const tinybench_1 = require("tinybench");
+let arr = [5, 4, 8, 10, 9, 3, 2, 1, 7, 6];
 let sortedArray = (0, mergesort_1.mergeSort)(arr);
 console.log("Original Array: ", arr);
 console.log("Merge Sort: ", sortedArray);
@@ -13,3 +23,29 @@ sortedArray = arraySort(arr);
 console.log("Array Sort: ", sortedArray);
 sortedArray = (0, bubblesort_1.bubbleSort)(arr);
 console.log("Bubble sort: ", sortedArray);
+function randomNumbers(n) {
+    const MAX_NUMBER = 100000;
+    const arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(Math.floor(Math.random() * MAX_NUMBER + 1));
+    }
+    return arr;
+}
+const bench = new tinybench_1.Bench();
+bench.add("Merge sort", () => (0, mergesort_1.mergeSort)(arr));
+bench.add("Array sort", () => arraySort(arr));
+bench.add("Bubble sort", () => (0, bubblesort_1.bubbleSort)(arr));
+let n = 0;
+(function () {
+    return __awaiter(this, void 0, void 0, function* () {
+        for (let i = 1; i <= 5; i++) {
+            n = 10 ** i;
+            arr = randomNumbers(n);
+            yield bench.run();
+        }
+    });
+})();
+bench.addEventListener("complete", () => {
+    console.log(`N = ${n}`);
+    console.table(bench.table());
+});
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import { mergeSort } from './mergesort';
 import { bubbleSort } from './bubblesort';
+import { Bench } from 'tinybench';
 
-const arr = [5, 4, 8, 10, 9, 3, 2, 1, 7, 6];
+let arr = [5, 4, 8, 10, 9, 3, 2, 1, 7, 6];
 
 let sortedArray = mergeSort(arr);
 
@@ -17,3 +18,33 @@ console.log("Array Sort: ", sortedArray);
 
 sortedArray = bubbleSort(arr);
 console.log("Bubble sort: ", sortedArray);
+
+function randomNumbers(n: number):number[] {
+    const MAX_NUMBER = 100000;
+    const arr: number[] = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(Math.floor(Math.random() * MAX_NUMBER + 1));
+    }
+    return arr;
+}
+
+const bench = new Bench();
+
+bench.add("Merge sort", () => mergeSort(arr));
+bench.add("Array sort", () => arraySort(arr));
+bench.add("Bubble sort", () => bubbleSort(arr));
+
+let n = 0;
+
+(async function() {
+  for (let i = 1; i <= 5; i++) {
+    n = 10**i;
+    arr = randomNumbers(n);
+    await bench.run();
+  }  
+})();
+
+bench.addEventListener("complete", () => {
+    console.log(`N = ${n}`);
+    console.table(bench.table());
+});
