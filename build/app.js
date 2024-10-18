a62440ae92cb751f81e65378a8ebbdcc76b6a9ba
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-10-18 17:08:17 +0100

    Add arraySort function

--- build/app.js
+++ build/app.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello World!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const mergesort_1 = require("./mergesort");
+const arr = [5, 4, 8, 10, 9, 3, 2, 1, 7, 6];
+let sortedArray = (0, mergesort_1.mergeSort)(arr);
+console.log("Original Array: ", arr);
+console.log("Merge Sort: ", sortedArray);
+function arraySort(arr) {
+    return arr.sort((a, b) => a - b);
+}
+sortedArray = arraySort(arr);
+console.log("Array Sort: ", sortedArray);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import { mergeSort } from './mergesort';
+
+const arr = [5, 4, 8, 10, 9, 3, 2, 1, 7, 6];
+
+let sortedArray = mergeSort(arr);
+
+console.log("Original Array: ", arr);
+console.log("Merge Sort: ", sortedArray);
+
+function arraySort(arr: number[]):number[] {
+    return arr.sort((a, b) => a - b);
+}
+
+sortedArray = arraySort(arr);
+console.log("Array Sort: ", sortedArray);
